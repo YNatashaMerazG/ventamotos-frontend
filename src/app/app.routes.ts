2970089f15f8fa7351b0cdba04e6d509ac20cbd7
Yessica Natasha Meraz <natashamerazg@gmail.com>
2026-02-22 06:09:48 -0800

    generación de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+//GENERACION DEL ENTURTAMIENTO
+//RENRUTAMIENTO: CLIC PARA CONDUCIR A ALGUN LADO DE LA PAGINA
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Products } from './pages/products/products';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+//Comenzamos a llamar a los componentes
+export const routes: Routes = [
+    //path: 'ruta', componente: donde vamos
+    {path: '', component: Home},
+    {path: 'nosotros', component: About},
+    {path: 'productos', component: Products},
+    {path: 'contacto', component: Contact},
+    //ruta no existente
+    {path: '**', redirectTo: ''} // redirectTo: componenteRaiz
+];
